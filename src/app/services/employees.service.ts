ae1a27105e6910d901bd13f4c02ba1bd4983cc1f
Yusdenys Perez <yudemendoza@gmail.com>
2022-06-30 21:10:33 -0400

    The edit-component was created

--- src/app/services/employees.service.ts
+++ src/app/services/employees.service.ts
@@ -22,5 +22,9 @@ export class EmployeesService {
     addEmployeeRequest.id = '00000000-0000-0000-0000-000000000000';
     return this.http.post<Employee>(this.baseApiUrl + '/api/employees', addEmployeeRequest);
   }
+
+  getEmployee(id: string): Observable<Employee>{
+    return this.http.get<Employee>(this.baseApiUrl + '/api/employees' + id);
+  }
 }
 
